4caa93097b8b6866dca22db81d3530a3026366c3
rmd09 <rmd09@yandex.ru>
2024-08-23 19:11:30 +0300

    feat: добавление учеников: кнопка и форма

--- app/components/StudentCardList/StudentCardList.jsx
+++ app/components/StudentCardList/StudentCardList.jsx
@@ -9,8 +9,14 @@ import { StudentInfo } from "../StudentInfo/StudentInfo";
 
 export const StudentCardList = (props) => {
     const [isStudentPopupOpened, setIsStudentPopupOpened] = useState(false);
+    const [isPopupForChanging, setIsPopupForChanging] = useState(false);
 
-    const openPopup = () => {
+    const openPopupForChanging = () => {
+        setIsPopupForChanging(true);
+        setIsStudentPopupOpened(true);
+    }
+    const openPopupForCreating = () => {
+        setIsPopupForChanging(false);
         setIsStudentPopupOpened(true);
     }
 
@@ -21,12 +27,20 @@ export const StudentCardList = (props) => {
     return (
         <>
         <main className={Styles["main"]}>
-            <h1 className={Styles["title"]}>Список учеников:</h1>
+            <section className={Styles["header"]}>
+                <h1 className={Styles["title"]}>Список учеников:</h1>
+                <div className={Styles["add__student__container"]}>
+                    <h1 className={Styles["add__student"]}>Добавить ученика</h1>
+                    <svg onClick={openPopupForCreating} className={`${Styles["plus-svg"]}`} width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M13 3C13 2.44772 12.5523 2 12 2C11.4477 2 11 2.44772 11 3V11H3C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H11V21C11 21.5523 11.4477 22 12 22C12.5523 22 13 21.5523 13 21V13H21C21.5523 13 22 12.5523 22 12C22 11.4477 21.5523 11 21 11H13V3Z" fill="#0F0F0F"/>
+                    </svg>
+                </div>
+            </section>
 
             <div className={Styles["card__list"]}></div>
             {
                 props.data?.map((student, key) => {
-                    return <StudentCard {...student} openPopup={openPopup} key={key}/>
+                    return <StudentCard {...student} openPopup={openPopupForChanging} key={key}/>
                 })
             }
         </main>
@@ -35,7 +49,7 @@ export const StudentCardList = (props) => {
             <>
             <Overlay close={closePopup} />
             <Popup close={closePopup}>
-                <StudentInfo />
+                <StudentInfo isForChanging={isPopupForChanging} />
             </Popup>
             </>
         )}
